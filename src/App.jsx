2361a5c861614990f34207678522847fe082b178
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2025-02-11 15:28:46 +0500

    initializing

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './components/Header'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div className='text-3xl font-bold underline'>App</div>
+    </>
+  )
+}
+
+export default App
