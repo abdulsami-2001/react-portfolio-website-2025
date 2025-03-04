ba099d11fef8a5fa120c8ee7aa56e95ff6399b43
Abdul Sami <abdulsamisiddiq2001@gmail.com>
2025-03-05 00:22:28 +0500

    arrange sequence of work n experience

--- src/components/WorkExperienceandEducation.jsx
+++ src/components/WorkExperienceandEducation.jsx
@@ -36,31 +36,6 @@ const WorkExperienceandEducation = () => {
                         <h4 className="text-md sm:text-lg text-gray-600">Fiverr</h4>
                         <p className="text-gray-700 text-sm sm:text-base">Remote · Freelance</p>
                     </VerticalTimelineElement>
-
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="June 2023 - September 2024"
-                        iconStyle={{ background: "#03b296", color: "#fff", boxShadow: "0 0 10px rgba(3, 178, 150, 0.5)" }}
-                        contentStyle={{ background: "#fff", borderRight: "4px solid #03b296", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", borderRadius: "12px", padding: "20px" }}
-                        contentArrowStyle={{ borderRight: "7px solid #03b296" }}
-                        icon={<Briefcase />}
-                    >
-                        <h3 className="text-xl sm:text-2xl font-semibold text-gray-900">React Native Internship</h3>
-                        <h4 className="text-md sm:text-lg text-gray-600">VentureDive</h4>
-                        <p className="text-gray-700 text-sm sm:text-base">Onsite · Full time · Mobile App Development, React Native</p>
-                    </VerticalTimelineElement>
-                    <VerticalTimelineElement
-                        className="vertical-timeline-element--work"
-                        date="October 2020 - September 2024"
-                        iconStyle={{ background: "#03b296", color: "#fff", boxShadow: "0 0 10px rgba(3, 178, 150, 0.5)" }}
-                        contentStyle={{ background: "#fff", borderLeft: "4px solid #03b296", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", borderRadius: "12px", padding: "20px" }}
-                        contentArrowStyle={{ borderRight: "7px solid #03b296" }}
-                        icon={<SchoolIcon />}
-                    >
-                        <h3 className="text-xl sm:text-2xl font-semibold text-gray-900">Bachelor of Science in Computer Science</h3>
-                        <h4 className="text-md sm:text-lg text-gray-600">Indus University</h4>
-                        <p className="text-gray-700 text-sm sm:text-base">Onsite · Bachelor Degree · Computer Science</p>
-                    </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
                         date="October 2023 - January 2024"
@@ -72,6 +47,32 @@ const WorkExperienceandEducation = () => {
                         <h3 className="text-xl sm:text-2xl font-semibold text-gray-900">Mobile App Development</h3>
                         <h4 className="text-md sm:text-lg text-gray-600">Hazza Institute of Technology</h4>
                         <p className="text-gray-700 text-sm sm:text-base">Onsite · Short Course · React Native</p>
+                    </VerticalTimelineElement>
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="June 2023 - September 2024"
+                        iconStyle={{ background: "#03b296", color: "#fff", boxShadow: "0 0 10px rgba(3, 178, 150, 0.5)" }}
+                        contentStyle={{ background: "#fff", borderLeft: "4px solid #03b296", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", borderRadius: "12px", padding: "20px" }}
+                        contentArrowStyle={{ borderRight: "7px solid #03b296" }}
+                        icon={<Briefcase />}
+                    >
+                        <h3 className="text-xl sm:text-2xl font-semibold text-gray-900">React Native Internship</h3>
+                        <h4 className="text-md sm:text-lg text-gray-600">VentureDive</h4>
+                        <p className="text-gray-700 text-sm sm:text-base">Onsite · Full time · Mobile App Development, React Native</p>
+                    </VerticalTimelineElement>
+
+
+                    <VerticalTimelineElement
+                        className="vertical-timeline-element--work"
+                        date="October 2020 - September 2024"
+                        iconStyle={{ background: "#03b296", color: "#fff", boxShadow: "0 0 10px rgba(3, 178, 150, 0.5)" }}
+                        contentStyle={{ background: "#fff", borderRight: "4px solid #03b296", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)", borderRadius: "12px", padding: "20px" }}
+                        contentArrowStyle={{ borderRight: "7px solid #03b296" }}
+                        icon={<SchoolIcon />}
+                    >
+                        <h3 className="text-xl sm:text-2xl font-semibold text-gray-900">Bachelor of Science in Computer Science</h3>
+                        <h4 className="text-md sm:text-lg text-gray-600">Indus University</h4>
+                        <p className="text-gray-700 text-sm sm:text-base">Onsite · Bachelor Degree · Computer Science</p>
                     </VerticalTimelineElement>
                     <VerticalTimelineElement
                         className="vertical-timeline-element--work"
